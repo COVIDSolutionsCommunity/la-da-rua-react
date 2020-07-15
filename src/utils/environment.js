ec887d0b650662ffacca333929a233f3f71c0dc3
Marina Tavares <marinastavares6@gmail.com>
2020-07-14 21:19:24 -0300

    fix: possible

--- src/utils/environment.js
+++ src/utils/environment.js
@@ -1,3 +1,2 @@
-export const API_URL =
-  process.env.REACT_APP_API_URL || 'http://api-ladarua-staging.herokuapp.com/api/v1/'
+export const API_URL = 'https://api-ladarua-staging.herokuapp.com/api/v1/'
 export const GOOGLE_ANALYTICS = 'UA-166261280-1'
